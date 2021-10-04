7ad9fed494f294e7a06c85048b1f8ab00c144897
Dan Goodman <dg.github@thesamovar.net>
2021-10-04 18:41:08 +0100

    closing sections

--- src/components/Flow.js
+++ src/components/Flow.js
@@ -3,26 +3,38 @@ import react from "react";
 class Flow extends react.Component {
     constructor(props) {
         super(props);
-        this.state = {visible_sections: ['', 'primary']};
-        this.sections = {'': <div dangerouslySetInnerHTML={{ __html: props.main.innerHTML }} />,
-                        'primary': this.buildSection('primary')};
+        this.state = {visible_sections: ['', 'primary', 'secondary']};
+        this.sections = {'': <div dangerouslySetInnerHTML={{ __html: props.main.innerHTML }} />};
+        document.querySelectorAll('section').forEach(elem => {
+            this.buildSection(elem.id);
+        });
     }
     buildSection(sec) {
         const sec_html = document.getElementById(sec).innerHTML;
         let tempdiv = document.createElement('div');
         tempdiv.innerHTML = sec_html;
         tempdiv.querySelectorAll('.notpaper-include').forEach(elem=>{
-            console.log(elem.getAttribute('href'));
             const target = document.querySelector(elem.getAttribute('href'));
-            console.log(target);
             elem.outerHTML = target.innerHTML;
         });
-        return <div dangerouslySetInnerHTML={{__html: tempdiv.innerHTML}}/>;
+        this.sections[sec] = <div dangerouslySetInnerHTML={{__html: tempdiv.innerHTML}}/>;
+    }
+    clickHeader(sec) {
+        let visible = this.state.visible_sections;
+        if(sec===visible[visible.length-1] && visible.length>1) {
+            visible.pop();
+            this.setState({visible_sections: visible});
+        } else {
+            const i = visible.findIndex((elem)=>elem===sec);
+            visible = visible.slice(0, i+1);
+            this.setState({visible_sections: visible});
+        }
     }
     render() {
         const headers = [];
         this.state.visible_sections.forEach(sec => {
-            headers.push(<button key={'flowbutton-'+sec}>{sec}</button>);
+            const label = sec==="" ? "Main article" : "Section: "+sec;
+            headers.push(<button key={'flowbutton-'+sec} onClick={(e)=>{this.clickHeader(sec)}}>{label}</button>);
         });
         return (
         <div id="FlowColumn" className="Column">
